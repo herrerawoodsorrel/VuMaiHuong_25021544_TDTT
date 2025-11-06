f1fcbe814a4049222538cece53dd97d762f52d71
herrerawoodsorrel <herrerawoodsorrel@gmail.com>
2025-11-06 15:56:53 +0700

    Create W2A3.py

---
+++ Thuchanh1.py/W2A3.py
@@ -0,0 +1,10 @@
+a=int(input("Nhap so nguyen a: "))
+b=int(input("Nhap so nguyen b: "))
+tong=a+b
+hieu=a-b
+tich=a*b
+thuong=a/b
+print("Tong:", tong)
+print("Hieu:", hieu)
+print("Tich:", tich)
+print("Thuong:", thuong)
